bce17eb288e658076d2fa24d40360dcea6c71ca5
Matt Craig <sonical2002@yahoo.com.au>
2020-09-28 22:31:08 +0800

    #2 Add console app demoing public API functionality

---
+++ DeribitDotNet.Demo/Level.cs
@@ -0,0 +1,22 @@
+﻿namespace DeribitDotNet.Demo
+{
+    public readonly struct Level
+    {
+        public readonly double Price;
+        public readonly int Quantity;
+
+        public Level(double price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString() => $"{Quantity}@{Price}";
+
+        public override bool Equals(object obj) =>
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            obj is Level other && Price == other.Price && Quantity == other.Quantity;
+
+        public override int GetHashCode() => (Price.GetHashCode() * 397) ^ Quantity.GetHashCode();
+    }
+}
